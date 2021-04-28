a69bff5280963673f21b926abfd6da1d8201d2c5
Lee_Coder <82861521+topgun0415@users.noreply.github.com>
2021-04-29 03:34:19 +0900

    Add files via upload

---
+++ JAVA/First Java Practice.java
@@ -0,0 +1,31 @@
+
+public class TryEverything {
+	public static void main(String[] args) {
+		
+		char q = 'D';
+		System.out.println("q =" + q);
+		
+		char b = '¾ß';
+		System.out.println(b);
+		
+		int c = 55;
+		System.out.println(c);
+		
+		int z = 55;
+		System.out.println("z =" + z);
+	
+		
+		double d = 0.256;
+		System.out.println("d =" + d);
+		
+		boolean f = true;
+		System.out.println("f =" + f);
+		
+		String a = "Hello my world!";
+		System.out.println(a);
+	
+		
+		
+	}
+
+}
