484faaa2ef5fe78c6ee701f81c32018811821506
Lee_Coder <82861521+topgun0415@users.noreply.github.com>
2021-05-12 18:35:18 +0900

    Add files via upload

---
+++ JAVA/12MAY21/Egoing7.java
@@ -0,0 +1,25 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Egoing7 {
+public static void main(String[] args) throws IOException {
+	
+	//What is class ?
+	 
+	 System.out.println(Math.PI);
+	 
+	System.out.println(Math.floor(1.6));
+	System.out.println(Math.ceil(1.6));
+	
+	
+   //Instance 
+	
+ PrintWriter p1 = new PrintWriter("result1.txt");
+ p1.write("Hello world! XD");
+ p1.close();
+		
+	
+  }	
+}
+
