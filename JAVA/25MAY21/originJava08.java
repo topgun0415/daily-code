c67d31629b97624d68ec5a8fd34559629686e7da
Lee_Coder <82861521+topgun0415@users.noreply.github.com>
2021-05-25 22:45:40 +0900

    Create originJava08.java

---
+++ JAVA/25MAY21/originJava08.java
@@ -0,0 +1,42 @@
+
+public class originJava08 {
+
+	public static void main(String[] args) {
+		
+
+		/* 논리형 = true or false  중 하나를 선택. 조건식 과 논리적 계산에 사용됨
+		 *  문자형 = 문자(Char)를 저장하는데 사용되며 , 여러가지 문자(String)을 저장할 수 있다. 
+		 *  정수형 = 정수 값을 저장하는데 사용되며, 주로 사용되는 것은 int or long.
+		 *  			   byte 는 이진 데이터를 사용하는데 쓰임 
+		 *                 short 는 c언어와의 호환을 위해 쓰이지만 자바에서는 많이 쓰이지않음.
+		 *                
+		 *  
+		 *  
+		 *  
+		 *  
+		 *  실수형 = 실수 값을 저장하는데 쓰인다. Float & double 이 있다.
+		 */ 
+		
+		// Println의 단점 
+		
+	    System.out.println(10/3.0);  // 실수의 자리수 조절불가
+		System.out.println(0x1A); // 오로지 10진수로만 출력가능
+		
+		// Printf
+		
+		System.out.printf("%.ef", 10.0/3);   // 3.33
+		System.out.printf("%d", 0X1A);   //  26
+		System.out.printf("%X", 0X1A);   // 1A
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
