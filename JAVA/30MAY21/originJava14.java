58383c75ac7f8dfc106daa01110a51248d8c7669
Lee_Coder <82861521+topgun0415@users.noreply.github.com>
2021-05-30 23:50:44 +0900

    Create originJava14.java

---
+++ JAVA/30MAY21/originJava14.java
@@ -0,0 +1,34 @@
+
+public class originJava14 {
+
+	
+	   public static void main(String[] args) {
+	
+	         //문자열의 비교
+		   
+             String str = "ABC";
+             String str1 = "ABC";
+             
+             System.out.println(str==str1);
+             System.out.println(str.equals(str1));
+             
+             String str2 = new String ("abc");
+             String str3 = new String ("abc");
+             
+             System.out.println(str2==str3);
+             System.out.println(str2.equals(str3));
+             
+             
+            // true
+            // true
+            // false
+            // true
+
+     
+     
+     
+     }
+
+
+
+}
