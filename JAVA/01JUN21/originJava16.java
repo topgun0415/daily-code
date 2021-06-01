b16cda249943a7832a55655e77baabd0c64dac98
Lee_Coder <82861521+topgun0415@users.noreply.github.com>
2021-06-01 23:45:34 +0900

    Create originJava16.java

---
+++ JAVA/01JUN21/originJava16.java
@@ -0,0 +1,33 @@
+
+public class originJava16 {
+
+	public static void main(String[] args) {
+		
+		char c = 'a';
+		for(int i =0; i<26; i++) {
+			System.out.print(c++);
+			
+			
+			
+		}
+		    System.out.println();       //줄바뀜 역활
+		    
+		 c = 'A';
+		 for(int i=0; i <10; i++) {
+			 System.out.print(c++);
+		
+		
+		
+		 }
+		 System.out.println();
+		 
+		 char lowerCase = 'A' ;
+		 char upperCase = (char)(lowerCase + 32);
+		 System.out.println(upperCase);
+		 
+	     
+
+		 
+	}
+
+}
