cfcc7a1a396da9777c0e6f38c512e8bf2487673c
Lee_Coder <82861521+topgun0415@users.noreply.github.com>
2021-06-05 23:33:23 +0900

    Create originJava21.java

---
+++ JAVA/05JUN21/originJava21.java
@@ -0,0 +1,24 @@
+
+public class originJava21 {
+
+	public static void main(String[] args) {
+		
+		// For문 (반복문) : 조건을 만족하는 동안 블럭 { }을 반복 
+		
+		// int i = 1; ( Variable's scope )
+		
+		
+		// 조건식을 생략하면, ture로 간주되어서 무한반복문이 됨
+		for (int i = 1; i <=7; i++)
+			System.out.println("I can do Java");
+		
+		
+		
+		
+
+		
+		
+
+	}
+
+}
