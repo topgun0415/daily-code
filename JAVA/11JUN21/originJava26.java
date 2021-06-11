3b5f63b3e74f70159321d2b162490dc5d59c51d3
Lee_Coder <82861521+topgun0415@users.noreply.github.com>
2021-06-11 14:08:30 +0900

    Create originJava26.java

---
+++ JAVA/11JUN21/originJava26.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class originJava26 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Please choose 1 or 0 ");
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		
+		if (input == 0) 
+		{
+			System.out.println("You chose 0");
+		}
+		else 
+		{
+			System.out.println("You chose 1");
+		}
+
+	}
+	
+}
