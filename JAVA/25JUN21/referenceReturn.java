2f16baca719fc70544f495e3ea50209e6617bfd1
Lee_Coder <82861521+topgun0415@users.noreply.github.com>
2021-06-25 23:50:38 +0900

    Create referenceReturn.java

---
+++ JAVA/25JUN21/referenceReturn.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class ch06practice {
+
+		public static void main (String args []) {
+			
+			ch06practice r = new ch06practice ();
+			
+			int result = r.add(3, 5);
+			System.out.println(result);
+			
+			int [] resu3 = {0}; //배열을 생성하고 result2[0]의 값을 0으로 초기화
+			r.add(3, 5, resu3); // 배열을 add메서드의 매개변수로 전달
+			System.out.println(resu3[0]);
+			
+		} //메인 끝
+ 
+		int add (int a, int b) { return a + b; }
+		
+		void add(int a, int b, int [] result) {result [0] = a + b;} //매개변수로 넘겨받은 배열에 연산결과를 저장
+			
+		
+		} //클래스 끝 
