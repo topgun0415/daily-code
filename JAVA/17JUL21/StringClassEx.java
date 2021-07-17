724f5d7e51391c41e9933babd8b112f3e9b35d10
Lee_Coder <82861521+topgun0415@users.noreply.github.com>
2021-07-17 22:58:54 +0900

    Create StringClassEx.java

---
+++ JAVA/17JUL21/StringClassEx.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String s = new String ("Hello");
+		System.out.println(s);
+		
+		char [ ]  c = { 'h' , 'e' , 'l' , 'l' , 'o' };
+		String a = new String (c);
+		System.out.println(a);
+		
+	}
+	
+}
+
+  
+
+
