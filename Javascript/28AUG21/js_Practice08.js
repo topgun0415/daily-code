dfd4a1e59dc8c326780e6d4ca019654fe22e9296
philipuuu <topgun0415@naver.com>
2021-08-29 00:48:02 +0900

    js_Practice08 final saved

---
+++ Javascript/28AUG21/js_Practice08.js
@@ -0,0 +1,28 @@
+// 2.5 while 반복문 
+
+/* 형식
+  while(조건식) {
+    실행문;
+    실행문;
+    실행문;
+  }
+*/
+
+// while(true) 를 넣어버리면 무한반복이 일어남 그렇기에 꼭 유한한 반복을 명령해야함.
+
+let i = 0;
+while (i <= 100) {
+  console.log("Hello, world!");
+  console.log(i)
+  i++;
+}
+
+
+
+
+
+
+
+
+
+
