b39b0ffa5f4522187ac58b3c9823555c8f8493c2
philipuuu <topgun0415@naver.com>
2021-10-01 18:18:19 +0900

    App react practice

--- React/ninja-dojo-blog/src/App.js
+++ React/ninja-dojo-blog/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new vlog";
+  const likes = 50;
+//const person = { name:"yoshi", age: 30 };
+  const google = "https://www.google.com/"
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        {/* <p> { person } </p> */}
+        <p>{ 10 }</p>
+        <p>{ "Hello ninja!" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10}</p>
+
+        <a href= { google }>Google Site</a>
+      </div>
     </div>
   );
 }
