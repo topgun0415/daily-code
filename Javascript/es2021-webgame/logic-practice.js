4e031bdb64e9ac5feb1ddfd715cea01eb1e1853d
philipuuu <topgun0415@naver.com>
2021-10-19 18:49:31 +0900

    Done : rsp-game completed

--- Javascript/es2021-webgame/logic-practice.js
+++ Javascript/es2021-webgame/logic-practice.js
@@ -1,14 +1,5 @@
 /** @format */
 
-const arr = ['R', 'E', 'A', 'O', 'Q'];
-const arr2 = [2, 6, 4, 8, 9, 16, 11];
-
-console.log(arr2.sort());
-console.log(arr.sort());
-
-const orderArr = arr.sort((a, b) => a < b);
-const arrNum = [5, 4, 10, 2, 9, 8];
-const orderNum = arrNum.sort((a, b) => b - a);
-
-for (var i = 0; i < 5; i++) {}
-console.log(i);
+const fun = (값) => () => {
+  console.log('고차 함수입니다', 2);
+};
