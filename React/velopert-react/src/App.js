f452e73ef745fa767da3ac6ab51eac78cfbd99b2
philipuuu <topgun0415@naver.com>
2021-10-29 16:47:59 +0900

    velopert-react D2 save

--- React/velopert-react/src/App.js
+++ React/velopert-react/src/App.js
@@ -1,32 +1,9 @@
 /** @format */
 import React from 'react';
-import './App.css';
+import MyNewComponent from './MyNewComponent';
 
-function App() {
-  const name = 'React JS';
-  const style3 = 'CSS + React';
-  const style2 = {
-    // background-color는 backgroundColor와 같이 파스칼 표기법이 아닌 카멜케이스로 변경됨
-    backgroundColor: 'black',
-    color: 'yellow',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 25,
-  };
-  return (
-    <div
-      style={{
-        // background-color는 backgroundColor와 같이 파스칼 표기법이 아닌 카멜케이스로 변경됨
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: '48px',
-        fontWeight: 'bold',
-        padding: 25,
-      }}>
-      {name}
-      <div style={style2}>React Ninja</div>
-      <div className='react'>{style3}</div>
-    </div>
-  );
-}
+const App = () => {
+  return <MyNewComponent favoriteNumber={3}>리액트</MyNewComponent>;
+};
+
 export default App;
