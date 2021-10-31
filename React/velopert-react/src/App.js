796594e336a26924e9b4c236a03017c7024dfdf5
philipuuu <topgun0415@naver.com>
2021-10-31 23:22:38 +0900

    velopert-react D4 saved

--- React/velopert-react/src/App.js
+++ React/velopert-react/src/App.js
@@ -1,9 +1,14 @@
 /** @format */
-import React from 'react';
-import EventPractice from './EventPractice';
 
-const App = () => {
-  return <EventPractice />;
-};
+// 문자열 타입으로 변환
 
-export default App;
+// 1. String 생성자 함수를 new 연산자 없이 호출하는 방법
+String(1);
+String(NaN);
+String(true); // boolean -> String
+
+// 2. Object.prototype.toString 메서드를 사용하는 방법
+
+(1).toString();
+NaN.toString();
+const type = true.toStrng();
