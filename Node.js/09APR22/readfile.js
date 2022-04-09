e7520572a4c0e2497367f3453b0de5acc497c90a
philipuuu <topgun0415@naver.com>
2022-04-10 01:06:00 +0900

    zero-nodejs02 save2

--- Node.js/09APR22/readfile.js
+++ Node.js/09APR22/readfile.js
@@ -2,10 +2,24 @@
 
 const fs = require('fs');
 
-fs.readFile('./readme.txt', (err, data) => {
+fs.readFile('Node.js/09APR22/readme.txt', (err, data) => {
   if (err) {
     throw err;
   }
-  console.log(data);
+  console.log(data); // 노드를 통해서는 읽을 수는 있으나 2진법으로 보여준다
   console.log(data.toString());
 });
+
+// Promise식으로도 가능
+
+const fsPromise = require('fs').promises;
+
+fsPromise
+  .readFile('Node.js/09APR22/readme.txt')
+  .then((data) => {
+    console.log(data);
+    console.log(data.toString());
+  })
+  .catch((err) => {
+    throw err;
+  });
