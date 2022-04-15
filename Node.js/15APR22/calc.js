0562b126686d16a4a095943d8badacf9277e5dca
philipuuu <topgun0415@naver.com>
2022-04-15 23:54:03 +0900

    winterlood node  save

---
+++ Node.js/15APR22/calc.js
@@ -0,0 +1,12 @@
+/** @format */
+
+// 계산 기능을 하는 파일
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+
+module.exports = {
+  moduleName: 'Calc module',
+  add: add,
+  sub: sub,
+};
---
+++ Node.js/15APR22/winter-lood.js
@@ -0,0 +1,10 @@
+/** @format */
+
+const calc = require('./calc');
+
+console.log(calc.add(1, 2)); // 3
+console.log(calc.add(4, 5)); // 9
+console.log(calc.sub(18, 2)); // 16
+
+// npm : node.js의 패키지 관리 도구 (남이 만든 package들을 공유하여 누구나 다 쓸 수 있게 하는 것)
+// package : 누군가 따로 만든 node 모듈,
