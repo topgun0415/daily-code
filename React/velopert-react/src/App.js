50744a613aa6b1412773c6210180f26ba4db96e4
philipuuu <topgun0415@naver.com>
2022-05-06 18:56:37 +0900

    🔥 Remove all the saves and start agian

--- React/velopert-react/src/App.js
+++ React/velopert-react/src/App.js
@@ -1,14 +1,13 @@
 /** @format */
 
-// 문자열 타입으로 변환
+import './App.css';
 
-// 1. String 생성자 함수를 new 연산자 없이 호출하는 방법
-String(1);
-String(NaN);
-String(true); // boolean -> String
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Velopert React</h1>
+    </div>
+  );
+};
 
-// 2. Object.prototype.toString 메서드를 사용하는 방법
-
-(1).toString();
-NaN.toString();
-const type = true.toString();
+export default App;
