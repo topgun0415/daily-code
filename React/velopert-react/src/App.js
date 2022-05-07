19a01d0468eae25b80788d56e766cfdd95f69d6d
philipuuu <topgun0415@naver.com>
2022-05-07 23:30:43 +0900

    Velopert React 00

--- React/velopert-react/src/App.js
+++ React/velopert-react/src/App.js
@@ -1,12 +1,26 @@
 /** @format */
 
+import React from 'react';
 import './App.css';
 
 const App = () => {
+  const user_name = 'Philip';
+  const user_status = 'admins';
+
   return (
-    <div className='App'>
-      <h1>Velopert React</h1>
-    </div>
+    <>
+      <div className='App'>
+        {user_name === 'Philip' ? (
+          <h1>Welcome to React Philip</h1>
+        ) : (
+          <h1>Hello to React World</h1>
+        )}
+        <h2>My name is {user_name}</h2>
+        <div>
+          {user_status === 'admins' && <h3>Welcome to Admins Pages</h3>}
+        </div>
+      </div>
+    </>
   );
 };
 
