2031fc584a75890ee94ee2bdd9587e47bd39adf6
philipuuu <topgun0415@naver.com>
2022-05-25 16:44:51 +0900

    velopert-react : basic JSX

--- React/velopert-react/src/App.js
+++ React/velopert-react/src/App.js
@@ -4,23 +4,40 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  const user_name = 'Philip';
+  const user_name = 'Philips';
   const user_status = 'admins';
+  const user_id = undefined;
+  const mainStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    width: 'auto',
+    height: '30vh',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+  };
 
   return (
-    <>
-      <div className='App'>
+    <div className='App'>
+      <div style={mainStyle}>
         {user_name === 'Philip' ? (
-          <h1>Welcome to React Philip</h1>
+          <div>Welcome to React Philip</div>
         ) : (
-          <h1>Hello to React World</h1>
+          <div>Hello to React World</div>
         )}
-        <h2>My name is {user_name}</h2>
-        <div>
-          {user_status === 'admins' && <h3>Welcome to Admins Pages</h3>}
-        </div>
+        <div>My name is {user_name}</div>
       </div>
-    </>
+      <div className='Admins'>
+        {user_status === 'admins' && <h3>Welcome to Admins Pages</h3>}
+        {/* undefined 혹은 falsy값 or 연산자로 오류 방지하기 */}
+        {user_id || 'Please change the User ID'}
+      </div>
+      <div>
+        <input type='text' />
+      </div>
+    </div>
   );
 };
 
