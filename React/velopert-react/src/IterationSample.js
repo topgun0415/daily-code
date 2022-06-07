eb697582051c5d6d732d9ea723569f245ed89c4e
philipuuu <topgun0415@naver.com>
2022-06-07 15:32:00 +0900

    Velopert React Map & Key Practice

--- React/velopert-react/src/IterationSample.js
+++ React/velopert-react/src/IterationSample.js
@@ -1,13 +1,52 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-  const names = ['눈사람', '얼음', '눈', '바람'];
-  const nameList = names.map((v, i) => <li key={i}>{v}</li>);
+  const [names, setNames] = useState([
+    { id: 1, text: 'snowMan' },
+    { id: 2, text: 'ice' },
+    { id: 3, text: 'fire' },
+    { id: 4, text: 'wind' },
+    { id: 5, text: 'grass' },
+  ]);
+
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(6); // 새로운 항목을 추가할 때 사용할 id
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+  const onClick = (e) => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText('');
+  };
+
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const nameList = names.map((name) => (
+    <li
+      key={name.id}
+      onDoubleClick={() => {
+        onRemove(name.id);
+        console.log('Delete proceed');
+      }}>
+      {name.text}
+    </li>
+  ));
 
   return (
     <div>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가하기</button>
       <ul>{nameList}</ul>
     </div>
   );
