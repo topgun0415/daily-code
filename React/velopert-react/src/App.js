a4ae7a8efbca3a62ac881f70dec3e17959b3bdf4
philipuuu <topgun0415@naver.com>
2022-07-31 23:44:32 +0900

    React LifeCycle pratice

--- React/velopert-react/src/App.js
+++ React/velopert-react/src/App.js
@@ -8,8 +8,23 @@ import EventPratice from './EventPratice';
 import ValidationSample from './ValidationSample';
 import IterationSample from './IterationSample';
 import HookPratice from './HookPratice';
+import LifeCycleSample from './LifeCycleSampe';
+
+const getRandomColor = () => {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+};
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     const name = 'react';
     return (
@@ -21,6 +36,9 @@ class App extends Component {
         <ValidationSample />
         <IterationSample />
         <HookPratice />
+        <h2>----- LifeCycle Practice ------</h2>
+        <button onClick={this.handleClick}>Random Color</button>
+        <LifeCycleSample color={this.state.color} />
       </div>
     );
   }
